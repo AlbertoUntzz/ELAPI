76fc58a9f9b6ed7003e73de074b7566b099e8ef5
AlbertoUntzz! <albertolpez13289@gmail.com>
2024-01-30 19:39:26 -0600

    Ya jalo x2

---
+++ src/Tenis.js
@@ -0,0 +1,17 @@
+[
+    {
+        "id": "1",
+        "Marca":"Nike",
+        "Modelo": "Air force one",
+        "Tallas": "25.5, 26, 27",
+        "Color": "Negro, Blanco",
+        "Precio":"2500"    
+    },
+    {
+        "id": "2",
+        "Modelo": "Jordan",
+        "Tallas": "25.5, 26, 27",
+        "Color": "Negro, Blanco",
+        "Precio":"2500"    
+    }
+]
